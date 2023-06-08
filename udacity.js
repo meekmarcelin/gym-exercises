e0db51e81aa1c79bdbee94bbd270c9e7dc9f3a47
meekmarcelin@gmail.com <meekmarcelin@gmail.com>
2023-06-08 14:57:53 +0200

    hhhhh

---
+++ udacity.js
@@ -0,0 +1,43 @@
+/*var my_string = "a";
+var ASCII_value = my_string.charCodeAt(0);
+console.log(ASCII_value);*/
+
+/*var my_string = "Udacity";
+
+// Iterate using a Loop
+for (var i = 0; i < my_string.length; i++) {
+  console.log(my_string.charCodeAt(i));
+}*/
+
+/*var poem = "Blowing from the west\nFallen leaves gather\nIn the east.";
+console.log(poem);*/
+
+/*var haiku = "Blowing from the west\nFallen leaves gather\nIn the east.";
+console.log(haiku);*/
+
+/*var haiku = "Blowing from the west + "\n" + Fallen leaves gather + "\n" + In the east.";
+console.log(haiku);*/
+/*var joke = "Why couldn't the shoes go out and play?\nThey were all 'tied' up!";
+console.log(joke);*/
+/*console.log(3 === 3);*/
+
+/*var thingOne = 2;
+var thingTwo = 6;
+console.log(thingOne + thingTwo);*/
+
+/*var adjective1 = "amazing";
+var adjective2 = "fun";
+var adjective3 = "entertaining";
+var sentence = "The Intro to JavaScript course is ${adjective1}. James and Julia are so ${adjective2}. I cannot wait to work through the rest of this ${adjective3}content!";
+console.log(sentence);*/
+/*function myFunction(a) {
+    return typeof a;
+  }*/
+
+  function arrayPlusArray(arr1, arr2) {
+    var sum;
+    for (let i = 0; i < arr1.length; i++){
+      sum += arr1[i];
+    }
+    console.log(sum);
+  }
